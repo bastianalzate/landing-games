527722ce6114dc5f88eb18050dfd206e30dbe408
Bastian Alzate <bastianmurilloalzate@gmail.com>
2023-04-27 22:26:39 -0500

    paralax effect

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,6 +20,15 @@ const slides = [
 
 const Hero = () => {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
 
     const prevSlide = () => {
         const newIndex = activeIndex === 0 ? slides.length - 1 : activeIndex - 1;
@@ -38,7 +47,11 @@ const Hero = () => {
         return () => clearInterval(intervalId);
     }, [activeIndex]);
 
+
+
     const { imageUrl, title, text } = slides[activeIndex];
+
+ 
 
     return (
         <div className="relative h-screen w-full overflow-hidden" style={{ height: 'calc(100vh - 80px)' }}>
@@ -48,12 +61,26 @@ const Hero = () => {
                     className={`absolute top-0 left-0 h-full w-full transition-opacity duration-1000 ${activeIndex === index ? 'opacity-100' : 'opacity-0'
                         }`}
                     style={{ backgroundImage: `url(${slide.imageUrl})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-                ></div>
+                >
+                    <div
+                        className={`absolute top-0 left-0 w-full h-full transition-all duration-500 ease-out ${isHovered ? 'scale-110' : 'scale-100'
+                            }`}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        style={{
+                            transformOrigin: 'center center',
+                            backgroundImage: `url(${slide.imageUrl})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            filter: 'brightness(0.7)',
+                        }}
+                    ></div>
+                </div>
             ))}
-           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-70 p-10 rounded-md">
-  <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold mb-6">{title}</h1>
-  <p className="text-white text-xl md:text-2xl lg:text-3xl">{text}</p>
-</div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-70 p-10 rounded-md">
+                <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold mb-6">{title}</h1>
+                <p className="text-white text-xl md:text-2xl lg:text-3xl">{text}</p>
+            </div>
 
             <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
                 {slides.map((_, index) => (
