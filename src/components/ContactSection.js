2d3bcbdf9f52a8ccdc499189c26179cf6cc91dc5
Bastian Alzate <bastianmurilloalzate@gmail.com>
2023-04-28 17:02:58 -0500

    add responsive in form contact

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -13,41 +13,39 @@ const ContactSection = () => {
   };
 
   return (
-    <section className="bg-gray-800 py-20">
+    <section className="bg-gray-800 py-10 md:py-20">
       <div className="max-w-screen-lg mx-auto px-4">
-        <h2 className="text-3xl font-bold text-white mb-8">Contáctanos</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div>
-              <label htmlFor="name" className="block text-gray-300 font-medium mb-2">
-                Nombre
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                required
-                className="w-full border-gray-600 border-2 rounded-md px-4 py-2 text-gray-500 focus:outline-none focus:border-blue-500"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-gray-300 font-medium mb-2">
-                Correo electrónico
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                className="w-full border-gray-600 border-2 rounded-md px-4 py-2 text-gray-500 focus:outline-none focus:border-blue-500"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
+        <h2 className="text-3xl font-bold text-white mb-6 md:mb-8">Contáctanos</h2>
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex flex-col">
+            <label htmlFor="name" className="block text-gray-300 font-medium mb-2">
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              className="border-gray-600 border-2 rounded-md px-4 py-2 text-gray-500 focus:outline-none focus:border-blue-500"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
-          <div className="mt-4">
+          <div className="flex flex-col">
+            <label htmlFor="email" className="block text-gray-300 font-medium mb-2">
+              Correo electrónico
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className="border-gray-600 border-2 rounded-md px-4 py-2 text-gray-500 focus:outline-none focus:border-blue-500"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col col-span-2">
             <label htmlFor="subject" className="block text-gray-300 font-medium mb-2">
               Asunto
             </label>
@@ -56,12 +54,12 @@ const ContactSection = () => {
               id="subject"
               name="subject"
               required
-              className="w-full border-gray-600 border-2 rounded-md px-4 py-2 text-gray-500 focus:outline-none focus:border-blue-500"
+              className="border-gray-600 border-2 rounded-md px-4 py-2 text-gray-500 focus:outline-none focus:border-blue-500"
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
             />
           </div>
-          <div className="mt-4">
+          <div className="flex flex-col col-span-2">
             <label htmlFor="message" className="block text-gray-300 font-medium mb-2">
               Mensaje
             </label>
@@ -69,12 +67,12 @@ const ContactSection = () => {
               id="message"
               name="message"
               required
-              className="w-full border-gray-600 border-2 rounded-md px-4 py-2 text-gray-500 h-48 focus:outline-none focus:border-blue-500"
+              className="border-gray-600 border-2 rounded-md px-4 py-2 text-gray-500 h-48 focus:outline-none focus:border-blue-500"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </div>
-          <div className="mt-8">
+          <div className="flex justify-center md:justify-start mt-6 md:mt-8">
             <button
               type="submit"
               className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out"
@@ -83,8 +81,7 @@ const ContactSection = () => {
             </button>
           </div>
         </form>
-     
-        </div>
+      </div>
     </section>
   );
 };
